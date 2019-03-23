967e1367c2ac7b64dc4031d0aabbc22ecf801b0b
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-03-23 23:42:25 +0700

    Refactor components folder

---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+export { default as Loader } from './Loader';
+export { default as HeaderSearch } from './HeaderSearch';
+export { default as HeaderNavigation } from './HeaderNavigation';
+export { default as Footer } from './Footer';
+export { default as SearchForm } from './SearchForm';
+export { default as BackToTop } from './BackToTop';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
+export { default as ForgetPassword } from './ForgetPassword';
+
---
+++ src/components/homePage/index.js
@@ -0,0 +1,8 @@
+export { default as Slider } from './Slider';
+export { default as LatestMovies } from './LatestMovies';
+export { default as UpcomingMovies } from './UpcomingMovies';
+export { default as HowItWorks } from './HowItWorks';
+export { default as Counter } from './Counter';
+export { default as BecomePremium } from './BecomePremium';
+export { default as Blog } from './Blog';
+export { default as Subscribe } from './Subscribe';
