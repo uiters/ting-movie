1aa076010e6b949043f3eb0451158fc556c1205d
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-15 13:20:31 +0700

    Update API sources (#51)

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
 export const fetchMovies = () => {
-  const params = {
-    param: {
-      url: '/film/list?status=2',
-      keyCache: 'showing-film'
-    },
-    method: 'GET'
-  };
-  return axios.post('/apitomapp', params, {
-    headers: { 'content-type': 'application/json' }
-  });
+  return axios.get('https://meomeocf98.000webhostapp.com/server/movies.php');
 };
--- src/api/sessions.js
+++ src/api/sessions.js
@@ -2,18 +2,11 @@ import axios from 'axios';
 import moment from 'moment';
 
 export const fetchSessions = movieId => {
-  const params = {
-    param: {
-      url: `/session/film?cinema_id=-1&film_id=${movieId}&start_date=${moment().format(
-        'YYYY-MM-DD'
-      )}&end_date=${moment()
-        .add(7, 'days')
-        .format('YYYY-MM-DD')}&location_id=1`,
-      keyCache: 'no-cache'
-    },
-    method: 'GET'
-  };
-  return axios.post('/apitomapp', params, {
-    headers: { 'content-type': 'application/json' }
-  });
+  const startDate = moment().format('YYYY-MM-DD');
+  const endDate = moment()
+    .add(7, 'days')
+    .format('YYYY-MM-DD');
+  return axios.get(
+    `https://meomeocf98.000webhostapp.com/server/sessions.php?cinemaID=-1&filmID=${movieId}&startDate=${startDate}&endDate=${endDate}`
+  );
 };
